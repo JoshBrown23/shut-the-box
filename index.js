12ccf4efd00077cdf800b5c3a338787826fb982e
Josh Brown <pokeboy99323@gmail.com>
2024-02-06 13:32:40 -0600

    Updating indexes

---
+++ index.js
@@ -0,0 +1,20 @@
+function RollDice() {
+    document.getElementById("die1").value = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("die2").value = Math.floor(Math.random() * 6) + 1;
+    
+    DoCalculations();
+}
+
+function DoCalculations() {
+    d1 = document.getElementById("die1").value;
+    d2 = document.getElementById("die2").value;
+
+    d1 = Number(d1);
+    d2 = Number(d2);
+
+    sum = d1 + d2;
+    calculation = "You can shut " + d1 + ", " + d2 + ", or " + sum + "<br>";
+
+    document.getElementById("calculations").innerHTML = calculation;
+}
+
